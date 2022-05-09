0392c3fcc3d51d3566b1a4884281d14351fba4f8
Juan Valdivia <jvaldivia@isaca.org>
2022-05-09 13:07:58 -0400

    type checks

--- src/components/IsacaHeader.tsx
+++ src/components/IsacaHeader.tsx
@@ -92,7 +92,7 @@ const styles = {
     alignItems: 'center',
     height: '106px',
     backgroundColor: '#e2e5e6',
-  },
+  } as React.CSSProperties,
   linkContainer: {
     textAlign: 'center',
     display: 'flex',
@@ -100,7 +100,7 @@ const styles = {
     justifyContent: 'center',
     height: '100%',
     width: '260px',
-  },
+  } as React.CSSProperties,
   activeLinkContainer: {
     textAlign: 'center',
     display: 'flex',
@@ -109,19 +109,19 @@ const styles = {
     height: '100%',
     width: '260px',
     borderBottom: '5px solid #a30046',
-  },
+  } as React.CSSProperties,
   link: {
     textDecoration: 'none',
     color: '#a30046',
     fontSize: '1.25em',
-  },
+  } as React.CSSProperties,
   activeLink: {
     textDecoration: 'none',
     color: 'black',
     cursor: 'default',
     pointerEvents: 'none',
     fontSize: '1.25em',
-  },
+  } as React.CSSProperties,
 };
 
 export default IsacaHeader;
