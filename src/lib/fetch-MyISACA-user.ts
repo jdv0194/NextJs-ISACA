74bd113abe39110f1be3ac2f9a8bd351e5312cb2
Juan Valdivia <jvaldivia@isaca.org>
2022-05-09 13:53:17 -0400

    fixing url

--- src/lib/fetch-MyISACA-user.ts
+++ src/lib/fetch-MyISACA-user.ts
@@ -1,7 +1,10 @@
+const baseURL = "https://cm-sit.isaca.org"
+
 export async function loadUser() {
     // Call an external API endpoint to get user
+    
     const res = await fetch(
-        'https://cm-sit.isaca.org/api/myisaca/jssheader?isacaId=467939'
+      `${baseURL}/api/myisaca/jssheader?isacaId=467939`
       );
     const data = await res.json()
     return data
@@ -10,7 +13,7 @@ export async function loadUser() {
   export async function loadUserSummary() {
     // Call an external API endpoint to get user summary
     const res = await fetch(
-        'https://cm-sit.isaca.org/api/myisaca/jsssummary?isacaId=467939'
+      `${baseURL}/api/myisaca/jssummary?isacaId=467939`
       );
     const data = await res.json()
     return data
